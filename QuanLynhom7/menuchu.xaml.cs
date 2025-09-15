9d2b8cd6fedd1cf254651495f1c5a0585659bbef
caoquangtuanhung2004 <caoquangtuanhung2004@gmail.com>
2025-09-15 16:46:35 +0700

    Mô tả thay đổi của bạn

---
+++ QuanLynhom7/menuchu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLynhom7
+{
+    /// <summary>
+    /// Interaction logic for menuchu.xaml
+    /// </summary>
+    public partial class menuchu : Window
+    {
+        public menuchu()
+        {
+            InitializeComponent();
+        }
+
+        private void quanlysinhvien_Click(object sender, RoutedEventArgs e)
+        {
+            quanlysinhvien sinhvienql = new quanlysinhvien();
+            sinhvienql.Show();
+            this.Close();
+
+        }
+    }
+}
---
+++ QuanLynhom7/quanlysinhvien.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLynhom7
+{
+    /// <summary>
+    /// Interaction logic for quanlysinhvien.xaml
+    /// </summary>
+    public partial class quanlysinhvien : Window
+    {
+        public quanlysinhvien()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+}
